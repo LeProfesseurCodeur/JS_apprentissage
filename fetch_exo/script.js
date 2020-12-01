ad069ebaaee464cffb00ee11f2b492df58af4702
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-12-01 11:11:00 +0100

    first commit with variable

---
+++ fetch_exo/script.js
@@ -0,0 +1,11 @@
+fetch('https://api.punkapi.com/v2/beers',{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: "Fuggles",
+    })
+}).then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
